d78f7f418d3d9bd3573e4ed5a7dcc9df9e5b2435
Torsten Blindert <torsten@Torstens-MacBook-Pro.local>
2014-12-31 00:41:24 +0100

    Initial commit

---
+++ blaze/global-imports.js
@@ -0,0 +1,12 @@
+/* Imports for global scope */
+
+Template = Template;
+Blaze = Blaze;
+UI = UI;
+Handlebars = Handlebars;
+Spacebars = Spacebars;
+ReactiveVar = Package['reactive-var'].ReactiveVar;
+SpacebarsCompiler = Package['spacebars-compiler'].SpacebarsCompiler;
+Meteor = Meteor;
+HTML = HTML;
+
